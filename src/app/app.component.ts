5c5c8ddd3d5b28237543d1d9d4fa75a1bbc6f0c0
clark.joao <46800211+clarkjoao@users.noreply.github.com>
2020-08-21 00:25:58 -0300

    Revert "Feat/FirebaseConfig"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,10 @@
 import { Component } from '@angular/core';
 
-// Firebase
-import { ApiService } from './services/api.service';
-import { AuthService } from './services/auth.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'daycare-manager';
-
-  constructor(private api: ApiService, private auth: AuthService) {}
-
-  ngOnInit() {
-    // this.auth
-    //   .create({
-    //     email: 'teste@none.com',
-    //     password: '123456',
-    //   })
-    //   .then((data: any) => {
-    //     console.log('sucess', data);
-    //     alert('success');
-    //   })
-    //   .catch((data: any) => {
-    //     console.log('error', data);
-    //     alert('error');
-    //   });
-  }
 }
